49897f41faed71a704c9701b150f864305e0857d
Elion Shahini <elion.shahini74@gmail.com>
2024-09-25 23:08:28 +0200

    searchbar mobile added

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,15 @@
+import SearchIcon from "../assets/search.png"
 
+import "./SearchBar.css"
 
 
 export default function SearchBar() {
  return (
-    <form>
-
-    </form>
+    <div className="searchbar">
+        <button className="buttonsearch">
+            <img className = "icon" src = {SearchIcon}/>
+        </button>
+        <input className="input-search" type = "text"/> 
+    </div>
   )
 }
